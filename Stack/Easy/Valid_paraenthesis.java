6d2da671304d09335f7efc6406b99173d8ac0cea
AnirudhaSawant <anirudhasawant02.as@gmail.com>
2024-12-19 22:05:36 +0530

    Stack Day - 25

---
+++ Stack/Easy/Valid_paraenthesis.java
@@ -0,0 +1,24 @@
+package Stack.Easy;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            if(!st.isEmpty()) {
+                char last = st.peek();
+                if(isPair(curr,last)) {
+                    st.pop();
+                    continue;
+                }
+            }
+            st.push(curr);
+        }
+        return st.isEmpty();
+    }
+
+    private boolean isPair(char curr, char last) {
+        return((last == '{' && curr == '}')|| (last == '(' && curr == ')') || (last == '[' && curr == ']'));
+    }
+}
