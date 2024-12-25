f2998821171de304e3252d8870a375b67a0f760c
AnirudhaSawant <anirudhasawant02.as@gmail.com>
2024-12-25 21:13:05 +0530

    Stack Day - 28

---
+++ Stack/Medium/Minimum_add_to_make_valid_parenthesis.java
@@ -0,0 +1,22 @@
+package Stack.Medium;
+
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0, close = 0;
+
+        for(char ch : s.toCharArray()) {
+            if(ch == '(') {
+                open++;
+            }
+            else{
+                if(open > 0) {
+                    open--;
+                }
+                else{
+                    close++;
+                }
+            }
+        }
+        return open + close;
+    }
+}
